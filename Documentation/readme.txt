931130399d17ed9a8d06db95ccfe91f1bd297b37
Vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-17 01:42:22 -0700

    Initial quick edits
    Minor edits now as ver 1.3 for compat usage with 1.8.x

---
+++ Documentation/readme.txt
@@ -0,0 +1,21 @@
+ * MyBB: Delete Account
+ *
+ * Authors: MyBB Security Group (Paul H.) & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.3
+
+
+ Installation:
+
+ 1. Transfer the files from /Upload/ directory to your MyBB Root directory
+ 
+ 2. Go to ACP -> Configuration -> Plugins and activate the plugin
+ 
+ 3. Go to ACP -> User & Groups -> Deleted Accounts -> Here you can view/restore all accounts deleted by the Delete Account plugin.
+
+
+Enjoy :D
+
+* version 1.3 for MyBB 1.8.x usage by Vintagedaddyo
---
+++ Upload/inc/languages/englishgb/deleteaccount.lang.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * MyBB: Delete Account
+ *
+ * File: deleteaccount.lang.php
+ *
+ * Authors: MyBB Security Group (Paul H.) & compat update by Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.3
+ *
+ */
+
+$l['da_are_you_sure'] = 'Are you sure you want to delete your account?  If so, continue.  If not, hit the back button on your browser or close the page.';
+
+$l['da_delete_account'] = 'Delete Account';
+
+$l['da_delete_account_name'] = 'Delete Account';
+
+$l['da_delete_reason'] = 'Reason for account deletion';
+
+$l['da_redirect'] = 'Your account has been deleted.  If you change your mind an administrator may be able to restore it for you.';
+
+$l['da_why'] = 'Please give us some information on why you are deleting your account.';
+
+$l['da_yes'] = 'Yes, I want to delete my account.';
+
+?>
