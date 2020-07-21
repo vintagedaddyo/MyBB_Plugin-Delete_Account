ba0bf5e0a7d0fc9f62126c7e599a1439aa084b92
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-21 16:00:31 -0700

    Update deleteaccount.lang.php

--- Upload/inc/languages/english/admin/deleteaccount.lang.php
+++ Upload/inc/languages/english/admin/deleteaccount.lang.php
@@ -14,15 +14,11 @@
 
 // English language pack for Delete Account plugin
 
-$l['da_are_you_sure'] = 'Are you sure you want to delete your account?  If so, continue.  If not, hit the back button on your browser or close the page.';
-
 $l['da_confirm_account_deletion'] = 'Are you sure you want to PERMANENTLY delete this account?  All information, including private messages, custom profile fields, etc, will be PERMANENTLY deleted.';
 
 $l['da_delete'] = 'Permanently Delete User';
 
 $l['da_deleted'] = 'Permanently deleted';
-
-$l['da_delete_account'] = 'Delete Account';
 
 $l['da_deleted_accounts'] = 'Deleted Accounts';
 
@@ -44,15 +40,9 @@ $l['da_permanently_deleted'] = 'The user has been permanently deleted.';
 
 $l['da_user_restored'] = 'User has been restored successfully!';
 
-$l['da_redirect'] = 'Your account has been deleted.  If you change your mind an administrator may be able to restore it for you.';
-
 $l['da_restore'] = 'Restore User';
 
 $l['da_restored'] = 'Restored user';
-
-$l['da_why'] = 'Please give us some information on why you are deleting your account.';
-
-$l['da_yes'] = 'Yes, I want to delete my account.';
 
 $l['da_settings_name'] = 'Delete Account Settings';
 
