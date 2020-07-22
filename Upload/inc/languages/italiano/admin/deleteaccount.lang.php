f7bd6b1d6dc9c1f16db23797cb959ca9ebd3914f
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-22 00:38:53 -0700

    Add files via upload

---
+++ Upload/inc/languages/italiano/admin/deleteaccount.lang.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * MyBB: Delete Account
+ *
+ * File: deleteaccount.lang.php
+ *
+ * Authors: MyBB Security Group (Paul H.) & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ */
+
+// Italiano language pack for Delete Account plugin
+
+$l['da_confirm_account_deletion'] = 'Sei sicuro di voler eliminare PERMANENTAMENTE questo account?  Tutte le informazioni, compresi i messaggi privati, i campi dei profili personalizzati, ecc., Verranno eliminate PERMANENTAMENTE.';
+
+$l['da_delete'] = 'Elimina definitivamente l utente';
+
+$l['da_deleted'] = 'Eliminato definitivamente';
+
+$l['da_deleted_accounts'] = 'Account eliminati';
+
+$l['da_deleted_accounts_desc'] = 'Qui puoi visualizzare / ripristinare tutti gli account eliminati dal plug-in Elimina account.';
+
+$l['da_delete_account_name'] = 'Eliminare l account';
+
+$l['da_delete_account_desc'] = 'Consenti agli utenti di eliminare il proprio account.  L account può essere ripristinato (o eliminato definitivamente) dagli amministratori.';
+
+$l['da_delete_reason'] = 'Motivo dell eliminazione dell account';
+
+$l['da_delete_time'] = 'Account cancellato il:';
+
+$l['da_invalid_user'] = 'Utente non valido';
+
+$l['da_no_deleted_accounts'] = 'Nessun account cancellato.';
+
+$l['da_permanently_deleted'] = 'L utente e stato eliminato definitivamente.';
+
+$l['da_user_restored'] = 'L utente e stato ripristinato correttamente!';
+
+$l['da_restore'] = 'Ripristina utente';
+
+$l['da_restored'] = 'Utente ripristinato';
+
+$l['da_settings_name'] = 'Elimina impostazioni account';
+
+$l['da_settings_desc'] = 'Impostazioni per il plug-in Elimina account.';
+
+$l['da_settings_normgroup_name'] = 'gruppi';
+
+$l['da_settings_normgroup_desc'] = 'Un CSV di gruppi che possono eliminare i loro account.';
+
+$l['da_settings_tpdeletion_name'] = 'Abilita l eliminazione di post / thread';
+
+$l['da_settings_tpdeletion_desc'] = 'Per impostazione predefinita, il processo di eliminazione permanente dell amministratore non rimuove i thread e i post degli utenti eliminati, ma li conserva e li contrassegna come l utente eliminato pubblicato come ospite.  Se si desidera abilitare il thread e pubblicare l eliminazione durante il processo di eliminazione permanente dell amministratore, e possibile abilitarlo qui selezionando Si o disabilitarlo selezionando No.';
+
+?>
