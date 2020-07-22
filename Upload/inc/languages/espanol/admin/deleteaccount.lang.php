fad78cdd0fa576ce98f70547cb867c08b85ecc5e
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-21 22:45:13 -0700

    Add files via upload

---
+++ Upload/inc/languages/espanol/admin/deleteaccount.lang.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * MyBB: Delete Account
+ *
+ * File: deleteaccount.lang.php
+ *
+ * Authors: MyBB Security Group (Paul H.) & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ */
+
+// Espanol language pack for Delete Account plugin
+
+$l['da_confirm_account_deletion'] = '¿Esta seguro de que desea eliminar PERMANENTEMENTE esta cuenta?  Toda la informacion, incluidos los mensajes privados, los campos de perfil personalizados, etc., se eliminaran PERMANENTEMENTE.';
+
+$l['da_delete'] = 'Eliminar usuario permanentemente';
+
+$l['da_deleted'] = 'Eliminado permanentemente';
+
+$l['da_deleted_accounts'] = 'Cuentas eliminadas';
+
+$l['da_deleted_accounts_desc'] = 'Aqui puede ver / restaurar todas las cuentas eliminadas por el complemento Eliminar cuenta.';
+
+$l['da_delete_account_name'] = 'Borrar cuenta';
+
+$l['da_delete_account_desc'] = 'Permitir a los usuarios eliminar su cuenta.  La cuenta puede ser restaurada (o eliminada permanentemente) por los administradores.';
+
+$l['da_delete_reason'] = 'Motivo de eliminacion de cuenta';
+
+$l['da_delete_time'] = 'Cuenta eliminada el:';
+
+$l['da_invalid_user'] = 'Usuario invalido';
+
+$l['da_no_deleted_accounts'] = 'No hay cuentas eliminadas.';
+
+$l['da_permanently_deleted'] = 'El usuario ha sido eliminado permanentemente.';
+
+$l['da_user_restored'] = '¡El usuario ha sido restaurado con exito!';
+
+$l['da_restore'] = 'Restaurar usuario';
+
+$l['da_restored'] = 'Usuario restaurado';
+
+$l['da_settings_name'] = 'Eliminar configuracion de cuenta';
+
+$l['da_settings_desc'] = 'Configuracion para el complemento Eliminar cuenta.';
+
+$l['da_settings_normgroup_name'] = 'Grupos';
+
+$l['da_settings_normgroup_desc'] = 'Un CSV de grupos que pueden eliminar sus cuentas.';
+
+$l['da_settings_tpdeletion_name'] = 'Habilitar la eliminacion de publicaciones / hilos';
+
+$l['da_settings_tpdeletion_desc'] = 'De forma predeterminada, el proceso de eliminacion permanente del administrador no elimina los hilos y publicaciones del usuario eliminado y, en cambio, los retiene y los marca como el usuario eliminado publicado como invitado.  Si desea habilitar la eliminacion de subprocesos y publicaciones durante el proceso de eliminacion permanente del administrador, puede habilitarla aqui seleccionando si o deshabilitarla seleccionando no.';
+
+?>
