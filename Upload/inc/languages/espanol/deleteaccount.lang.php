f9725ab377e7f74646334d4ecacfc97b956e7b98
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-21 22:46:47 -0700

    Add files via upload

---
+++ Upload/inc/languages/espanol/deleteaccount.lang.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * MyBB: Delete Account
+ *
+ * File: deleteaccount.lang.php
+ *
+ * Authors: MyBB Security Group (Paul H.) & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ */
+
+// Espanol language pack for Delete Account plugin
+
+$l['da_are_you_sure'] = '¿Estas seguro de que deseas eliminar tu cuenta?  Si es asi, continua.  De lo contrario, presione el boton Atras en su navegador o cierre la pagina.';
+
+$l['da_are_you_sure_note'] = 'Tenga en cuenta que está eligiendo que se elimine su cuenta.  Si cambia de opinion, un administrador puede restaurarlo por solicitud, aunque si desea la eliminacion permanente de la cuenta sin ninguna opción para la restauracion de la cuenta, indíquelo en su solicitud de motivo de eliminacion a continuacion.  (Tenga en cuenta tambien que si desea eliminar por completo los hilos y publicaciones de la cuenta en dicha solicitud de eliminacion permanente, indíquelo también en su solicitud de motivo de eliminacion).';
+
+$l['da_delete_account'] = 'Borrar cuenta';
+
+$l['da_delete_account_name'] = 'Borrar cuenta';
+
+$l['da_delete_reason'] = 'Motivo de eliminacion de cuenta';
+
+$l['da_redirect'] = 'Tu cuenta ha sido eliminada.  Si cambia de opinion, un administrador puede restaurarlo por usted.';
+
+$l['da_why'] = 'Danos informacion sobre por que estas eliminando tu cuenta.';
+
+$l['da_yes'] = 'Si, quiero eliminar mi cuenta.';
+
+?>
