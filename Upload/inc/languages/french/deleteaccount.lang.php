98512a727c20bb7c9bd91f2aaafff2becffb8bf8
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-21 23:29:20 -0700

    Add files via upload

---
+++ Upload/inc/languages/french/deleteaccount.lang.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * MyBB: Delete Account
+ *
+ * File: deleteaccount.lang.php
+ *
+ * Authors: MyBB Security Group (Paul H.) & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ */
+
+// French language pack for Delete Account plugin
+
+$l['da_are_you_sure'] = 'Voulez-vous vraiment supprimer votre compte?  Si oui, continuez.  Sinon, appuyez sur le bouton Precedent de votre navigateur ou fermez la page.';
+
+$l['da_are_you_sure_note'] = 'Veuillez noter que vous choisissez de supprimer votre compte.  Si vous changez d avis, un administrateur peut etre en mesure de le restaurer pour vous sur demande, mais si vous souhaitez une suppression permanente du compte sans option de restauration de compte, veuillez l indiquer dans votre demande de motif de suppression ci-dessous.  (Notez egalement que si vous souhaitez la suppression complete des fils de discussion et des publications de compte dans une telle demande de suppression permanente, veuillez egalement l indiquer dans votre demande de motif de suppression.)';
+
+$l['da_delete_account'] = 'Supprimer le compte';
+
+$l['da_delete_account_name'] = 'Supprimer le compte';
+
+$l['da_delete_reason'] = 'Raison de la suppression du compte';
+
+$l['da_redirect'] = 'Votre compte a ete supprime.  Si vous changez d avis, un administrateur pourra peut-etre le restaurer pour vous.';
+
+$l['da_why'] = 'Veuillez nous expliquer pourquoi vous supprimez votre compte.';
+
+$l['da_yes'] = 'Oui, je souhaite supprimer mon compte.';
+
+?>
