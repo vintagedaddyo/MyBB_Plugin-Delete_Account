6e62c7288ae4002e2875f71af7dbb6e6c841726a
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-22 00:40:43 -0700

    Add files via upload

--- Upload/inc/languages/italiano/admin/deleteaccount.lang.php
+++ Upload/inc/languages/italiano/admin/deleteaccount.lang.php
@@ -14,20 +14,46 @@
 
 // Italiano language pack for Delete Account plugin
 
-$l['da_are_you_sure'] = 'Sei sicuro di voler cancellare il tuo account?  In tal caso, continua.  In caso contrario, premi il pulsante Indietro sul browser o chiudi la pagina.';
+$l['da_confirm_account_deletion'] = 'Sei sicuro di voler eliminare PERMANENTAMENTE questo account?  Tutte le informazioni, compresi i messaggi privati, i campi dei profili personalizzati, ecc., Verranno eliminate PERMANENTAMENTE.';
 
-$l['da_are_you_sure_note'] = 'Tieni presente che stai scegliendo di eliminare il tuo account.  Se cambi idea, un amministratore potrebbe essere in grado di ripristinarlo su richiesta, anche se desideri cancellare definitivamente l account senza alcuna opzione per il ripristino dell account, indicalo nella richiesta del motivo della cancellazione di seguito.  (Si noti inoltre che se si desidera la completa rimozione delle discussioni e dei post dell account in tale richiesta di eliminazione permanente, si prega di dichiararlo anche nella richiesta del motivo di eliminazione.)';
+$l['da_delete'] = 'Elimina definitivamente l utente';
 
-$l['da_delete_account'] = 'Eliminare l account';
+$l['da_deleted'] = 'Eliminato definitivamente';
+
+$l['da_deleted_accounts'] = 'Account eliminati';
+
+$l['da_deleted_accounts_desc'] = 'Qui puoi visualizzare / ripristinare tutti gli account eliminati dal plug-in Elimina account.';
 
 $l['da_delete_account_name'] = 'Eliminare l account';
 
+$l['da_delete_account_desc'] = 'Consenti agli utenti di eliminare il proprio account.  L account può essere ripristinato (o eliminato definitivamente) dagli amministratori.';
+
 $l['da_delete_reason'] = 'Motivo dell eliminazione dell account';
 
-$l['da_redirect'] = 'Il tuo account e stato cancellato  Se cambi idea, un amministratore potrebbe essere in grado di ripristinarlo.';
+$l['da_delete_time'] = 'Account cancellato il:';
 
-$l['da_why'] = 'Forniscici alcune informazioni sul motivo per cui stai eliminando il tuo account.';
+$l['da_invalid_user'] = 'Utente non valido';
 
-$l['da_yes'] = 'Si, desidero cancellare il mio account.';
+$l['da_no_deleted_accounts'] = 'Nessun account cancellato.';
+
+$l['da_permanently_deleted'] = 'L utente e stato eliminato definitivamente.';
+
+$l['da_user_restored'] = 'L utente e stato ripristinato correttamente!';
+
+$l['da_restore'] = 'Ripristina utente';
+
+$l['da_restored'] = 'Utente ripristinato';
+
+$l['da_settings_name'] = 'Elimina impostazioni account';
+
+$l['da_settings_desc'] = 'Impostazioni per il plug-in Elimina account.';
+
+$l['da_settings_normgroup_name'] = 'gruppi';
+
+$l['da_settings_normgroup_desc'] = 'Un CSV di gruppi che possono eliminare i loro account.';
+
+$l['da_settings_tpdeletion_name'] = 'Abilita l eliminazione di post / thread';
+
+$l['da_settings_tpdeletion_desc'] = 'Per impostazione predefinita, il processo di eliminazione permanente dell amministratore non rimuove i thread e i post degli utenti eliminati, ma li conserva e li contrassegna come l utente eliminato pubblicato come ospite.  Se si desidera abilitare il thread e pubblicare l eliminazione durante il processo di eliminazione permanente dell amministratore, e possibile abilitarlo qui selezionando Si o disabilitarlo selezionando No.';
 
 ?>
