9ec4e2e0cfdfc91981fa3190e97e57c89a4fd554
vintagedaddyo <vintagedaddyo@gmail.com>
2020-07-21 23:28:21 -0700

    Add files via upload

---
+++ Upload/inc/languages/french/admin/deleteaccount.lang.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * MyBB: Delete Account
+ *
+ * File: deleteaccount.lang.php
+ *
+ * Authors: MyBB Security Group (Paul H.) & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ */
+
+// French language pack for Delete Account plugin
+
+$l['da_confirm_account_deletion'] = 'Etes-vous sur de vouloir supprimer definitivement ce compte?  Toutes les informations, y compris les messages prives, les champs de profil personnalises, etc., seront definitivement supprimees.';
+
+$l['da_delete'] = 'Supprimer definitivement l utilisateur';
+
+$l['da_deleted'] = 'Supprime definitivement';
+
+$l['da_deleted_accounts'] = 'Comptes supprimes';
+
+$l['da_deleted_accounts_desc'] = 'Ici, vous pouvez afficher / restaurer tous les comptes supprimes par le plugin Supprimer le compte.';
+
+$l['da_delete_account_name'] = 'Supprimer le compte';
+
+$l['da_delete_account_desc'] = 'Autoriser les utilisateurs a supprimer leur compte.  Le compte peut etre restaure (ou supprime definitivement) par les administrateurs.';
+
+$l['da_delete_reason'] = 'Raison de la suppression du compte';
+
+$l['da_delete_time'] = 'Compte supprime le:';
+
+$l['da_invalid_user'] = 'Utilisateur invalide';
+
+$l['da_no_deleted_accounts'] = 'Aucun compte supprime.';
+
+$l['da_permanently_deleted'] = 'L utilisateur a ete definitivement supprime.';
+
+$l['da_user_restored'] = 'L utilisateur a ete restaure avec succes!';
+
+$l['da_restore'] = 'Restaurer l utilisateur';
+
+$l['da_restored'] = 'Utilisateur restaure';
+
+$l['da_settings_name'] = 'Supprimer les parametres du compte';
+
+$l['da_settings_desc'] = 'Parametres du plug-in Supprimer le compte.';
+
+$l['da_settings_normgroup_name'] = 'Groupes';
+
+$l['da_settings_normgroup_desc'] = 'Un CSV de groupes qui peuvent supprimer leurs comptes.';
+
+$l['da_settings_tpdeletion_name'] = 'Activer la suppression des messages / discussions';
+
+$l['da_settings_tpdeletion_desc'] = 'Par defaut, le processus de suppression permanente de l administrateur ne supprime pas les fils de discussion et les publications utilisateur supprimes, mais les conserve et les marque comme l utilisateur supprime publie en tant qu invite.  Si vous souhaitez activer la suppression des fils et des publications pendant le processus de suppression permanente de l administrateur, vous pouvez l activer ici en selectionnant oui ou le desactiver en selectionnant non.';
+
+?>
